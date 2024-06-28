73e642fe6290fda6594c55e49389c0ab773f467f
MuhammadUzairAbbasi <136907020+MuhammadUzairAbbasi@users.noreply.github.com>
2024-06-28 06:15:38 +0000

    Profile Page Updated

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -4,17 +4,30 @@ import CommonHeader from '../../components/Common/CommonHeader'
 import './Profile.css'
 
 const Profile = () => {
-  const user={
-    userName:"Uzair",
-    email:"abbasiuzr@gmail.com"
+  const user = {
+    userName: "Uzair",
+    email: "abbasiuzr@gmail.com"
   }
   return (
     <div>
-      <CommonHeader title={user && user.userName }/>
+      <CommonHeader title={user && user.userName} />
       <div className="avatar">
-        <Avatar className='avatar__profile__pic'/>
+        <Avatar className='avatar__profile__pic' />
       </div>
-      
+
+      <div className='Profile_Informaton'>
+        <div >
+          <h5>Your Profile Information</h5>
+          <div className='profile_option'></div>
+        </div>
+        <div>
+          <h5>Profile</h5>
+          <div className="profile_info">
+            </div>
+        </div>
+
+      </div>
+
     </div>
   )
 }
