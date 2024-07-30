c39d64eed6d0dfe14ac6258f05ecb6dede8e8de4
MuhammadUzairAbbasi <muhammaduzair25k@gmail.com>
2024-07-31 00:07:55 +0500

    Notification model integerated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Link, NavLink } from "react-router-dom";
-import { IconButton, Avatar } from "@mui/material";
+import { Link } from "react-router-dom";
+import { IconButton, Avatar, Menu, MenuItem, Badge, Fade } from "@mui/material";
 import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
 import logo from "../../assets/Logo.jpg";
 import axios from "axios";
@@ -12,24 +12,58 @@ const Header = () => {
   const { user } = useContext(UserContext);
   const [image, setImage] = useState("");
   const [userdata, setUserData] = useState("");
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [notifications, setNotifications] = useState([]);
 
   useEffect(() => {
-    // Fetch the initial user data here if needed
     const fetchUser = async () => {
       try {
         const response = await axios.get(
           `${baseServerUrl}/api/users/profile/${user._id}`
         );
-        // console.log(response.data);
         setUserData(response.data);
         setImage(response.data.profilePicture);
       } catch (error) {
         console.error("Error fetching user data:", error);
       }
     };
-
     fetchUser();
   }, [userdata]);
+
+  useEffect(() => {
+    const fetchNotifications = async () => {
+      try {
+        const response = await axios.get(
+          `${baseServerUrl}/api/notifications/getnotifications`
+        );
+        setNotifications(
+          response.data.filter((notification) => !notification.read)
+        );
+      } catch (error) {
+        console.error("Error fetching notifications:", error);
+      }
+    };
+    fetchNotifications();
+  }, [notifications]);
+
+  const handleNotificationClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleNotificationItemClick = async (id) => {
+    try {
+      await axios.put(`${baseServerUrl}/api/notifications/markasread/${id}`);
+      setNotifications(
+        notifications.filter((notification) => notification._id !== id)
+      );
+    } catch (error) {
+      console.error("Error marking notification as read:", error);
+    }
+  };
 
   return (
     <div className="header">
@@ -39,9 +73,32 @@ const Header = () => {
           <h5>Edu Hub</h5>
         </div>
         <div className="right__header">
-          <IconButton>
-            <NotificationsActiveIcon />
+          <IconButton onClick={handleNotificationClick}>
+            <Badge badgeContent={notifications.length} color="secondary">
+              <NotificationsActiveIcon />
+            </Badge>
           </IconButton>
+          <Menu
+            id="notification-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            TransitionComponent={Fade}
+          >
+            {notifications.length === 0 ? (
+              <MenuItem onClick={handleClose}>No new notifications</MenuItem>
+            ) : (
+              notifications.map((notification, index) => (
+                <MenuItem
+                  key={index}
+                  onClick={() => handleNotificationItemClick(notification._id)}
+                >
+                  {notification.message}
+                </MenuItem>
+              ))
+            )}
+          </Menu>
           <Link to="/profile">
             <Avatar src={image} alt={user.username} />
           </Link>
